a760bdbb3e513dbaa2b4b1d296375542060eac8b
Alexandra <Lexxiecole@gmail.com>
2018-09-07 02:03:25 -0400

    YE

--- js/js.js
+++ js/js.js
@@ -1,2 +1,10 @@
-var apiKey = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p&limit=5");
+
+//doesnt work?vlook into json
+var apiKey = $("#apiKey").get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p&limit=5");
 xhr.done(function(data) { console.log("success got data", data); });
+
+
+
+
+
+
