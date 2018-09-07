66d0903d5dafac2b4a902463579e880861320d5b
Alexandra <Lexxiecole@gmail.com>
2018-09-07 02:38:04 -0400

    lol well apparently, document.ready doesnt want to work anymore, so thats really fun. its correct. ugh

--- js/js.js
+++ js/js.js
@@ -1,10 +1,10 @@
 
 //doesnt work?vlook into json
-var apiKey = $("#apiKey").get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+//var apiKey = $("#apiKey").get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p&limit=5");
+//xhr.done(function(data) { console.log("success got data", data); });
 
-
-
-
-
-
+$(document).ready(function() {
+    $("#skeleton").on("click", function() {
+        console.log("work omg")
+    })
+})
