2022ce7c35f5e92c5f6254663bc307f7b2547055
Alexandra <Lexxiecole@gmail.com>
2018-09-10 15:08:41 -0400

    gotta learn how to make an api key

--- js/js.js
+++ js/js.js
@@ -1,12 +1,47 @@
+var apiKey = dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p
+
+
+
+
+
+
+
 
 //doesnt work?vlook into json
 //var apiKey = $("#apiKey").get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dEUGYQ1Cuyu4NeTiq89sfuDLXX8RPA1p&limit=5");
 //xhr.done(function(data) { console.log("success got data", data); });
 
 $(document).ready(function() {
-    
+
     $("#skeleton").on("click", function() {
-        console.log("work omg")
+        console.log("Skeleton")
+    })
+    $("#boat").on("click", function() {
+        console.log("Boat")
+    })
+    $("#celebrate").on("click", function() {
+        console.log("Celebrate")
+    })
+    $("#travel").on("click", function() {
+        console.log("Travel")
+    })
+    $("#sunday").on("click", function() {
+        console.log("Sunday")
+    })
+    $("#birthday").on("click", function() {
+        console.log("Birthday")
+    })
+    $("#laugh").on("click", function() {
+        console.log("Laugh")
+    })
+    $("#smile").on("click", function() {
+        console.log("Smile")
+    })
+    $("#explore").on("click", function() {
+        console.log("Explore")
+    })
+    $("#sun").on("click", function() {
+        console.log("Sun")
     })
 })
 
